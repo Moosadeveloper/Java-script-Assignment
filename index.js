6a3be59de76acaad74c394e225b5006b3d1078be
Moosadeveloper <janmoosa77@gmail.com>
2024-06-04 22:20:31 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+ 
+ var z =prompt("enter your roll number")
+ let num =["1","3","9","5","6","10","11","14","18"]
+ for(var i = 0; i <=0; i++){
+    if(z===num[i]){
+        alert("you are passed")
+    }
+    else{
+        alert(" Sorry You are fail");
+    }
+ }
+  
